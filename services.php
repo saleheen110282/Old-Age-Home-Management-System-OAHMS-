3b20b9bc61a5c388dfc45e9373611ca39bf3c263
Asib177 <mdrakibulasibredoy@gmail.com>
2025-04-28 10:04:04 +0600

    Services Initil ccommit

--- services.php
+++ services.php
@@ -4,23 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Services</title>
+    <link rel="stylesheet" href="includes/css/services.css">
     <style>
         *{
             margin: 0px;
             padding: 0px;
         }
     </style>
-    <?php
-        include_once('includes/font.php');
-    ?>
 </head>
 <body>
     <?php
         include_once('includes/nav.php');
     ?>
-    <h1>Services Page</h1>
-    
-
+    <div id="div_top">
+        <h2 id="div_top_text">Services</h2>
+        <p id="div_top_paragraph">We provide world-class service for the elderly people. Good Will is now providing two services.</p>
+    </div>
+    <div id="div_bottom">
+        <div id="div_bottom_left">
+            <div id="free_box">
+                <table class="table_text">
+                    <tr>
+                        <td class="hedding_text">Free</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+                <button class="btn">Start</button>
+            </div>
+        </div>
+        <div id="div_bottom_right">
+            <div id="paid_box">
+                <table>
+                    <tr>
+                        <td class="hedding_text">Paid</td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+                <button class="btn">Start</button>
+            </div>
+        </div>
+    </div>
 
     <?php
         include_once('includes/footer.php');
