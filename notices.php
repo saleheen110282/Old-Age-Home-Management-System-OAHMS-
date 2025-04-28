2f8618d7f43689175ea65c2bb310b554dd29dc56
Asib177 <mdrakibulasibredoy@gmail.com>
2025-04-28 14:11:00 +0600

    Notice Pge Done

--- notices.php
+++ notices.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notice | Good Will Old Age Home</title>
+    <link rel="stylesheet" href="includes/css/notice.css">
     <style>
         *{
             margin: 0px;
@@ -15,9 +16,53 @@
     <?php
         include_once('includes/nav.php');
     ?>
-    <h1>Notices Page</h1>
-    
-
+    <div id="div_top">
+        <h2 id="div_top_text">Notice</h2>
+        <p id="div_top_paragraph">Here are the notice of the organization about key information, upcoming events and essential organizational announcement.</p>
+    </div>
+    <div class="container">
+        <div class="notices">
+            <div class="notice-card">
+                <div class="date">
+                    <span>📅 12/03/25</span>
+                </div>
+            <div class="title">Renovation of the rooms</div>
+            </div>          
+            <div class="notice-card">
+                <div class="date">
+                    <span>📅 12/03/25</span>
+                </div>
+                <div class="title">Renovation of the rooms</div>
+            </div>
+            <div class="notice-card">
+                <div class="date">
+                    <span>📅 12/03/25</span>
+                </div>
+                <div class="title">Renovation of the rooms</div>
+            </div>
+            <div class="notice-card active">
+                <div class="date">
+                    <span>📅 12/03/25</span>
+                </div>
+                <div class="title">Renovation of the rooms</div>
+            </div>
+        </div>
+        <div class="pinned-notice">
+            <div class="pin-icon">📌</div>
+            <div class="pinned-title">Pinned Notice</div>
+            <div class="date">
+                <span>📅 12/03/25</span>
+            </div>
+            <div class="title">Renovation of the rooms</div>
+        </div>
+    </div>
+    <div class="pagination">
+        <button disabled>Previous</button>
+        <button class="page active">1</button>
+        <span>...</span>
+        <button class="page">3</button>
+        <button>Next</button>
+    </div>
 
     <?php
         include_once('includes/footer.php');
