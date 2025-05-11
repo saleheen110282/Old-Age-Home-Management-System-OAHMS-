3c6e62db695b432a13471dfed8428cfe435d4edb
Asib177 <mdrakibulasibredoy@gmail.com>
2025-05-11 12:36:48 +0600

    Contact Page

--- contact.php
+++ contact.php
@@ -23,7 +23,26 @@
     <p id="div_top_paragraph">Send your message to us.</p>
   </div>
   <div id="div_bottom">
-    Bottom
+    <div id="div_bottom_figure">
+      <div class="form_group">
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" required placeholder="Enter your full name">
+        </div>
+        <div class="form_group">
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required placeholder="Email address">
+        </div>
+        <div class="form_group">
+          <label for="phone">Phone Number</label>
+          <input type="tel" id="phone" name="phone" required placeholder="01814-XXXXXX">
+        </div>
+        <div class="form_group">
+          <label for="message">Message</label>
+          <textarea id="message" name="message" required placeholder="Type your message here..."></textarea>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+      </div>
+    </div>
   </div>
 
     <?php
